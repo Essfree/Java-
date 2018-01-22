e098b19e608855e0c89db7084060d687d80bbe2d
Essfree <essfree@163.com>
2018-01-22 14:27:42 +0800

    第五章例题施工完成，第五章习题施工中

---
+++ src/cpt5/Example.java
@@ -0,0 +1,64 @@
+package cpt5;
+
+import java.util.Scanner;
+
+public class Example {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+//		判断回文
+//		String string = in.nextLine();
+//		int i = 0;
+//		int j = string.length()-1;
+//		while(i<=j){
+//			if(string.charAt(i) != string.charAt(j)){
+//				System.out.println("this is not ");
+//				break;
+//			}
+//			i++;
+//			j--;
+//		}
+//		if(i>j){
+//			System.out.println("this is");
+//		}
+		
+//		输出前50个素数
+//		final int numberOfPrimes = 50;
+//		final int numberOfPerLine = 10;
+//		int count = 0;
+//		int number = 2;
+//		while(count<numberOfPrimes){
+//			boolean isPrime = true;
+//			for(int i = 2;i<=number/2;i++){
+//				if(number % i == 0){
+//					isPrime = false;
+//					break;
+//				}
+//			}
+//			if (isPrime) {
+//				count++;
+//				if(count%numberOfPerLine == 0){
+//					System.out.println(number);
+//				}else{
+//					System.out.print(number+" ");
+//				}
+//			}
+//			number++;
+//		}
+		
+//		判断是否为素数
+//		int i;
+//		int number = in.nextInt();
+//		for(i = 2;i<number/2;i++){
+//			if(number%i == 0){
+//				System.out.println("is not Prime");
+//				break;
+//			}
+//		}
+//		if(i == number/2){
+//			System.out.println("is prime");
+//		}
+		
+	}
+
+}
---
+++ src/cpt5/Exercise.java
@@ -0,0 +1,35 @@
+package cpt5;
+
+import java.util.Scanner;
+
+public class Exercise {
+
+	public static void main(String[] args) {
+
+		Scanner in = new Scanner(System.in);
+//		ex 1		
+//		int input=0;
+//		int countPlus = 0;
+//		int countMinus = 0;
+//		double total = 0;
+//		double average = 0;
+//		input = in.nextInt();
+//		while (input!=0){
+//			total +=input;
+//			if(input>0){
+//				countPlus++;
+//			}else{
+//				countMinus++;
+//			}
+//			input = in.nextInt();
+//		}
+//		average = (double)(total)/(countMinus+countPlus);
+//		System.out.println(countPlus);
+//		System.out.println(countMinus);
+//		System.out.println(total);
+//		System.out.println(average);
+		
+		
+	}
+
+}
