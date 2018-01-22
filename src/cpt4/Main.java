dea9c5266c2822ba0f36060f821e1bb4f70d74b6
Essfree <essfree@163.com>
2018-01-22 11:16:18 +0800

    第四章完成

--- src/cpt4/Main.java
+++ src/cpt4/Main.java
@@ -1,5 +1,6 @@
 package cpt4;
 
+import java.text.DecimalFormat;
 import java.util.Scanner;
 
 public class Main {
@@ -29,9 +30,116 @@ public class Main {
 //	    System.out.println("The distance between the two points is " + d + " km");
 	
 //		ex5
-		System.out.print("Enter the number of sides : ");
-		System.out.print("Enter the side : ");
+//		System.out.print("Enter the number of sides : ");
+//		int n = in.nextInt();
+//		System.out.print("Enter the side : ");
+//		double s = in.nextDouble();
+//		double area = (double)(n*s*s)/(4*Math.tan((double)Math.PI/n));
+//		System.out.println("The area of the polygon is "+ area);
+	
+//		ex7
+//		double r = 100;
+//		System.out.print("Enter the radius of the bounding circle : "+r);
+//		for (int i = 0; i < 5; i++) {
+//			double a = Math.random() * 2 * Math.PI;
+//			double x = r * Math.cos(a);
+//			double y = r * Math.sin(a);
+//			System.out.println("("+x+" , "+y+")");
+//		}
+		
+//		ex8 ASCII码的转换
+//		int ascii = ;
+//		char ch = (char)ascii;
+//		System.out.println(ch);
+		
+//		ex9 给出字符的unicode码
+//		String m = in.nextLine();
+//		int t = (int)(m.charAt(0));
+//		System.out.println(t);
+//		
+//		ex11   十进制转换成十六进制
+//		int decimal;
+//		System.out.print("Enter a decimal value(0 to 15) :");
+//		decimal = in.nextInt();
+//		char hex;
+//		if(decimal<10){
+//			System.out.println("The hex value is "+decimal);
+//		}else if(decimal>=10&&decimal<=15){
+//			hex = (char) (decimal+'A'-10);
+//			System.out.println("The hex value is "+hex);
+//		}else{
+//			System.out.println(decimal+" is an invalid input"); 
+//		}
+//		
+//		ex 12	十六转二
+//		System.out.print("ENTER A HEX DIGIT : ");
+//		char hex = in.nextLine().charAt(0);
+//		int deci = hex-'A'+10;
+//		int binary = 0;
+//		int i = 0;
+//		while(deci!=0){
+//			binary += (int)(deci%2)*(Math.pow(10, i));
+//			deci = deci/2;
+//			i++;
+//		}
+//		System.out.println(binary);
+//		
+//		ex_16	
+//		int random = (int)(Math.random()*26);
+//		System.out.println(random);
+//		char rad = (char) (random+'A');
+//		System.out.println(rad);
+		
+//		ex_21
+//		String m = in.nextLine();
+//		int i = 0;
+//		if (m.length()!=11 || m.charAt(3)!='-' || m.charAt(6)!='-') {
+//			System.out.println(m+" is an invalid social security number");
+//		}else{
+//			for(i = 0;i<m.length()&&i!=3&&1!=6;i++){
+//				if (m.charAt(i)<48||m.charAt(i)>57) {
+//					break;
+//				}
+//			}
+//			if (i == m.length()) {
+//				System.out.println(m+" is a valid social security number");
+//			}else{
+//				System.out.println(m+" is an invalid social security number");
+//				
+//			}
+//		}
+		
+//		ex 22
+//		String s1 = in.nextLine();
+//		String s2 = in.nextLine();
+//		if(s1.contains(s2)){
+//			System.out.println(s2+" is a substring of "+s1);
+//		}else{
+//			System.out.println(s2+" is not a substring of "+s1);
+//		}
+//		ex 24
+//		String s1 = in.nextLine();
+//		String s2 = in.nextLine();
+//		String s3 = in.nextLine();
+//		if(s1.compareTo(s2)>0){
+//			String temp = s1;
+//			s1 = s2;
+//			s2 = temp;
+//		}
+//		if(s1.compareTo(s3)>0){
+//			String temp = s1;
+//			s1 = s3;
+//			s3 = temp;
+//		}
+//		if(s2.compareTo(s3)>0){
+//			String temp = s2;
+//			s2 = s3;
+//			s3 = temp;
+//		}
+//		System.out.println(s1+" "+s2+" "+s3);
+		
+		
 		
 	}
-	
+		
 }
